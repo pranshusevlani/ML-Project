fc92a46a64091aed963521fb506334a1e27d601d
g1gasad <asadahsan2245@gmail.com>
2024-04-19 11:29:09 +0530

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -14,7 +14,6 @@ from src.exception import CustomException
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-        
         os.makedirs(dir_path, exist_ok=True)
         
         with open(file_path, 'wb') as file_obj:
@@ -45,5 +44,14 @@ def evaluate_models(X_train, X_test, y_train, y_test, models, param):
             report[model_name] = test_r2_score
             
         return report
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+    
+def load_object(file_path):
+    try: 
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e, sys)
